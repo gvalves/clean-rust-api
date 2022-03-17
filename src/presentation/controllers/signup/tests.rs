ccaf6de49d4e43c9215dea49e91c9256ff764f5c
gvalves <gva@geekcode.com.br>
2022-03-17 15:22:20 -0300

    test: :white_check_mark: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup/tests.rs
+++ src/presentation/controllers/signup/tests.rs
@@ -36,3 +36,21 @@ pub async fn returns_400_if_no_name_is_provided() {
         &SignUpResBody::Err(ErrorMsg::new("missing param 'name'"))
     );
 }
+
+#[tokio::test]
+pub async fn returns_400_if_no_email_is_provided() {
+    let sut = SignUpController::new();
+    let body = SignUpReqBodyBuilder::new()
+        .set_name("any_name")
+        .set_password("any_password")
+        .set_password_confirmation("any_password")
+        .build();
+    let req = HttpRequest::new(Some(body));
+    let res = sut.handle(req).await;
+
+    assert_eq!(res.status_code(), 400);
+    assert_eq!(
+        res.body(),
+        &SignUpResBody::Err(ErrorMsg::new("missing param 'email'"))
+    );
+}
