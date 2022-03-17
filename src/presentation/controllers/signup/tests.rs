ee1a5544fcabd1a4c70f7da6f1868d142df63468
gvalves <gva@geekcode.com.br>
2022-03-17 15:39:11 -0300

    test: :white_check_mark: ensure SignUpController returns 400 if password confirmation fails

--- src/presentation/controllers/signup/tests.rs
+++ src/presentation/controllers/signup/tests.rs
@@ -86,6 +86,25 @@ pub async fn returns_400_if_no_password_confirmation_is_provided() {
     assert_eq!(res.status_code(), 400);
     assert_eq!(
         res.body(),
-        &SignUpResBody::Err(ErrorMsg::new("missing param 'password confirmation'"))
+        &SignUpResBody::Err(ErrorMsg::new("missing param 'password_confirmation'"))
+    );
+}
+
+#[tokio::test]
+pub async fn returns_400_if_password_confirmation_fails() {
+    let sut = SignUpController::new();
+    let body = SignUpReqBodyBuilder::new()
+        .set_name("any_name")
+        .set_email("any_email@mail.com")
+        .set_password("any_password")
+        .set_password_confirmation("invalid_password")
+        .build();
+    let req = HttpRequest::new(Some(body));
+    let res = sut.handle(req).await;
+
+    assert_eq!(res.status_code(), 400);
+    assert_eq!(
+        res.body(),
+        &SignUpResBody::Err(ErrorMsg::new("invalid param 'password_confirmation'"))
     );
 }
