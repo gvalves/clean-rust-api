09136bd2965b6fe9e559820587dd5b9c7e8ee6c9
gvalves <gva@geekcode.com.br>
2022-03-17 16:11:42 -0300

    test: :white_check_mark: ensure SignUpController returns 400 if invalid email is provided

--- src/presentation/controllers/signup/tests.rs
+++ src/presentation/controllers/signup/tests.rs
@@ -1,14 +1,47 @@
 use tokio;
 
-use crate::presentation::http::HttpRequest;
 use crate::presentation::protocols::controller::ControllerProtocol;
+use crate::presentation::{http::HttpRequest, protocols::email_validator::EmailValidator};
 use crate::ErrorMsg;
 
 use super::{SignUpController, SignUpReqBodyBuilder, SignUpResBody};
 
+fn make_sut() -> SignUpController {
+    let email_validator = make_email_validator();
+    SignUpController::new(email_validator)
+}
+
+fn make_email_validator() -> Box<dyn EmailValidator> {
+    make_email_validator_strategy(|_| true)
+}
+
+fn make_email_validator_strategy<T>(strategy: T) -> Box<dyn EmailValidator>
+where
+    T: Fn(&str) -> bool + Sync + 'static,
+{
+    struct EmailValidatorStub<T>
+    where
+        T: Fn(&str) -> bool + Sync,
+    {
+        strategy: T,
+    }
+
+    impl<T> EmailValidator for EmailValidatorStub<T>
+    where
+        T: Fn(&str) -> bool + Sync,
+    {
+        fn is_valid(&self, email: &str) -> bool {
+            let strategy = &self.strategy;
+            strategy(email)
+        }
+    }
+
+    Box::new(EmailValidatorStub { strategy })
+}
+
 #[tokio::test]
 pub async fn returns_400_if_req_body_is_none() {
-    let sut = SignUpController::new();
+    let sut = make_sut();
     let req = HttpRequest::new(None);
     let res = sut.handle(req).await;
 
@@ -21,7 +54,7 @@ pub async fn returns_400_if_req_body_is_none() {
 
 #[tokio::test]
 pub async fn returns_400_if_no_name_is_provided() {
-    let sut = SignUpController::new();
+    let sut = make_sut();
     let body = SignUpReqBodyBuilder::new()
         .set_email("any_email@mail.com")
         .set_password("any_password")
@@ -39,7 +72,7 @@ pub async fn returns_400_if_no_name_is_provided() {
 
 #[tokio::test]
 pub async fn returns_400_if_no_email_is_provided() {
-    let sut = SignUpController::new();
+    let sut = make_sut();
     let body = SignUpReqBodyBuilder::new()
         .set_name("any_name")
         .set_password("any_password")
@@ -57,7 +90,7 @@ pub async fn returns_400_if_no_email_is_provided() {
 
 #[tokio::test]
 pub async fn returns_400_if_no_password_is_provided() {
-    let sut = SignUpController::new();
+    let sut = make_sut();
     let body = SignUpReqBodyBuilder::new()
         .set_name("any_name")
         .set_email("any_email@mail.com")
@@ -74,7 +107,7 @@ pub async fn returns_400_if_no_password_is_provided() {
 }
 #[tokio::test]
 pub async fn returns_400_if_no_password_confirmation_is_provided() {
-    let sut = SignUpController::new();
+    let sut = make_sut();
     let body = SignUpReqBodyBuilder::new()
         .set_name("any_name")
         .set_email("any_email@mail.com")
@@ -92,7 +125,7 @@ pub async fn returns_400_if_no_password_confirmation_is_provided() {
 
 #[tokio::test]
 pub async fn returns_400_if_password_confirmation_fails() {
-    let sut = SignUpController::new();
+    let sut = make_sut();
     let body = SignUpReqBodyBuilder::new()
         .set_name("any_name")
         .set_email("any_email@mail.com")
@@ -106,5 +139,27 @@ pub async fn returns_400_if_password_confirmation_fails() {
     assert_eq!(
         res.body(),
         &SignUpResBody::Err(ErrorMsg::new("invalid param 'password_confirmation'"))
+    );
+}
+
+#[tokio::test]
+pub async fn returns_400_if_invalid_email_is_provided() {
+    let mut sut = make_sut();
+    sut.set_email_validator(make_email_validator_strategy(|_| false));
+
+    let body = SignUpReqBodyBuilder::new()
+        .set_name("any_name")
+        .set_email("invalid_email@mail.com")
+        .set_password("any_password")
+        .set_password_confirmation("any_password")
+        .build();
+
+    let req = HttpRequest::new(Some(body));
+    let res = sut.handle(req).await;
+
+    assert_eq!(res.status_code(), 400);
+    assert_eq!(
+        res.body(),
+        &SignUpResBody::Err(ErrorMsg::new("invalid param 'email'"))
     );
 }
