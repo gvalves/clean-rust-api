5aa0a134f58e4934f0fd012b77cfbdb49308e608
gvalves <gva@geekcode.com.br>
2022-03-18 00:46:40 -0300

    test: :white_check_mark: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email_validator_adapter/tests.rs
+++ src/utils/email_validator_adapter/tests.rs
@@ -1,10 +1,60 @@
 use crate::presentation::protocols::email_validator::EmailValidator;
 
-use super::EmailValidatorAdapter;
+struct EmailValidatorAdapterMock {
+    strategy: Box<dyn EmailValidator>,
+}
+
+impl EmailValidatorAdapterMock {
+    fn new() -> Self {
+        Self {
+            strategy: Box::new(EmailValidatorAlwaysTrueStrategy),
+        }
+    }
+
+    /// Set the email validator adapter mock's strategy.
+    fn set_strategy(&mut self, strategy: Box<dyn EmailValidator>) {
+        self.strategy = strategy;
+    }
+}
+
+impl EmailValidator for EmailValidatorAdapterMock {
+    fn is_valid(&self, email: &str) -> Result<bool, Box<dyn std::error::Error>> {
+        self.strategy.is_valid(email)
+    }
+}
+
+struct EmailValidatorAlwaysTrueStrategy;
+
+impl EmailValidator for EmailValidatorAlwaysTrueStrategy {
+    fn is_valid(&self, _: &str) -> Result<bool, Box<dyn std::error::Error>> {
+        Ok(true)
+    }
+}
+
+struct EmailValidatorAlwaysFalseStrategy;
+
+impl EmailValidator for EmailValidatorAlwaysFalseStrategy {
+    fn is_valid(&self, _: &str) -> Result<bool, Box<dyn std::error::Error>> {
+        Ok(false)
+    }
+}
+
+fn make_sut() -> EmailValidatorAdapterMock {
+    EmailValidatorAdapterMock::new()
+}
 
 #[test]
 fn returns_false_if_validator_returns_false() {
-    let sut = EmailValidatorAdapter;
+    let mut sut = make_sut();
+    sut.set_strategy(Box::new(EmailValidatorAlwaysFalseStrategy));
+
     let is_valid = sut.is_valid("invalid_email@mail.com").unwrap();
     assert!(!is_valid);
+}
+
+#[test]
+fn returns_true_if_validator_returns_true() {
+    let sut = make_sut();
+    let is_valid = sut.is_valid("valid_email@mail.com").unwrap();
+    assert!(is_valid);
 }
