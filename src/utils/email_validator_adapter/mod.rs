d1016eac9affd4b79caf69d224c349d2733894df
gvalves <gva@geekcode.com.br>
2022-03-18 00:46:24 -0300

    feat: :sparkles: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email_validator_adapter/mod.rs
+++ src/utils/email_validator_adapter/mod.rs
@@ -1,3 +1,5 @@
+use validator::Validate;
+
 use crate::presentation::protocols::email_validator::EmailValidator;
 
 #[cfg(test)]
@@ -7,6 +9,15 @@ pub struct EmailValidatorAdapter;
 
 impl EmailValidator for EmailValidatorAdapter {
     fn is_valid(&self, email: &str) -> Result<bool, Box<dyn std::error::Error>> {
-        Ok(false)
+        let email = String::from(email);
+        let data = DataToValidate { email };
+
+        Ok(data.validate().is_ok())
     }
+}
+
+#[derive(Validate)]
+struct DataToValidate {
+    #[validate(email)]
+    email: String,
 }
