7b73bb0ed1eb5e40b0f928f3b8775f77867cbaf5
gvalves <gva@geekcode.com.br>
2022-03-21 02:06:29 -0300

    refactor: :recycle: move tests for method add into module add

--- src/infra/db/account_mongo_repository/tests.rs
+++ src/infra/db/account_mongo_repository/tests.rs
@@ -1,11 +1,5 @@
-use mockall::predicate;
-
+use crate::domain::entities::account::AccountEntity;
 use crate::domain::usecases::add_account::AddAccountDto;
-use crate::ErrorMsg;
-use crate::{
-    data::protocols::add_account_repository::AddAccountRepository,
-    domain::entities::account::AccountEntity,
-};
 
 use super::{AccountMongoRepository, MockStdAccountRepository};
 
@@ -37,76 +31,87 @@ fn make_repository() -> Box<MockStdAccountRepository> {
     Box::new(MockStdAccountRepository::default())
 }
 
-#[tokio::test]
-async fn calls_repository_implementation_with_correct_data() {
-    let mut repository = make_repository();
-    repository
-        .expect_add()
-        .once()
-        .with(predicate::eq(AddAccountDto {
+mod add {
+    use mockall::predicate;
+
+    use crate::data::protocols::add_account_repository::AddAccountRepository;
+    use crate::domain::entities::account::AccountEntity;
+    use crate::domain::usecases::add_account::AddAccountDto;
+    use crate::ErrorMsg;
+
+    use super::{make_repository, make_sut};
+
+    #[tokio::test]
+    async fn calls_repository_implementation_with_correct_data() {
+        let mut repository = make_repository();
+        repository
+            .expect_add()
+            .once()
+            .with(predicate::eq(AddAccountDto {
+                name: String::from("valid_name"),
+                email: String::from("valid_email@mail.com"),
+                password: String::from("valid_password"),
+            }))
+            .returning(repository_add_default!());
+
+        let mut sut = make_sut();
+        sut.set_repository(repository);
+
+        let account_dto = AddAccountDto {
             name: String::from("valid_name"),
             email: String::from("valid_email@mail.com"),
             password: String::from("valid_password"),
-        }))
-        .returning(repository_add_default!());
+        };
 
-    let mut sut = make_sut();
-    sut.set_repository(repository);
-
-    let account_dto = AddAccountDto {
-        name: String::from("valid_name"),
-        email: String::from("valid_email@mail.com"),
-        password: String::from("valid_password"),
-    };
-
-    match sut.add(account_dto).await {
-        Ok(_) => {}
-        Err(_) => {}
+        match sut.add(account_dto).await {
+            Ok(_) => {}
+            Err(_) => {}
+        }
     }
-}
 
-#[tokio::test]
-async fn returns_err_if_repository_implementation_returns_err() {
-    let mut repository = make_repository();
-    repository
-        .expect_add()
-        .returning(|_| ErrorMsg::default().into());
+    #[tokio::test]
+    async fn returns_err_if_repository_implementation_returns_err() {
+        let mut repository = make_repository();
+        repository
+            .expect_add()
+            .returning(|_| ErrorMsg::default().into());
 
-    let mut sut = make_sut();
-    sut.set_repository(repository);
+        let mut sut = make_sut();
+        sut.set_repository(repository);
 
-    let account_dto = AddAccountDto {
-        name: String::from("valid_name"),
-        email: String::from("valid_email@mail.com"),
-        password: String::from("valid_password"),
-    };
+        let account_dto = AddAccountDto {
+            name: String::from("valid_name"),
+            email: String::from("valid_email@mail.com"),
+            password: String::from("valid_password"),
+        };
 
-    let result = sut.add(account_dto).await;
+        let result = sut.add(account_dto).await;
 
-    assert_eq!(
-        result.unwrap_err().to_string(),
-        ErrorMsg::default().to_string()
-    );
-}
+        assert_eq!(
+            result.unwrap_err().to_string(),
+            ErrorMsg::default().to_string()
+        );
+    }
 
-#[tokio::test]
-async fn returns_an_account_on_success() {
-    let sut = make_sut();
+    #[tokio::test]
+    async fn returns_an_account_on_success() {
+        let sut = make_sut();
 
-    let account_dto = AddAccountDto {
-        name: String::from("valid_name"),
-        email: String::from("valid_email@mail.com"),
-        password: String::from("valid_password"),
-    };
+        let account_dto = AddAccountDto {
+            name: String::from("valid_name"),
+            email: String::from("valid_email@mail.com"),
+            password: String::from("valid_password"),
+        };
 
-    let result = sut.add(account_dto).await;
+        let result = sut.add(account_dto).await;
 
-    if let Ok(account) = result {
-        assert_eq!(account.id(), "valid_id");
-        assert_eq!(account.name(), "valid_name");
-        assert_eq!(account.email(), "valid_email@mail.com");
-        assert_eq!(account.password(), "valid_password");
-    } else {
-        assert!(false);
+        if let Ok(account) = result {
+            assert_eq!(account.id(), "valid_id");
+            assert_eq!(account.name(), "valid_name");
+            assert_eq!(account.email(), "valid_email@mail.com");
+            assert_eq!(account.password(), "valid_password");
+        } else {
+            assert!(false);
+        }
     }
 }
